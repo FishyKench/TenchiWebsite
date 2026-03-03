f01db37fffbbac01b2a694963709f37dda07fa90
Mohammed Alhammaly <m7md.al7male@gmail.com>
2026-03-03 11:42:24 +0300

    inital commit (User,Game models and CRUD)

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<IGameManager, GameManager>();
 //builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("TestDb"));
--- obj/Debug/net9.0/TenchiWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/TenchiWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TenchiWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+936e7abc27c6854059a048d27d609791f1c4492b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TenchiWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TenchiWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
