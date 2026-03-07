02c412897348e6c86c3fea501f012b2025053bbe
Mohammed Alhammaly <m7md.al7male@gmail.com>
2026-03-07 09:47:35 +0300

    modified DTO requests to support data annotation

--- DTOs/CreateGameRequestDTO.cs
+++ DTOs/CreateGameRequestDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 public class CreateGameRequestDTO
 {
+    [Required]
+    [MaxLength(40)]
     public string GameTitle {get; set;} = string.Empty;
     public string Description {get; set;} = string.Empty;
     public DateTime ReleaseDate {get; set;}
+    [Required]
     public Status Status {get; set;} 
+    [Url]
     public string CoverImageUrl {get; set;} = string.Empty;
 }
--- DTOs/RegisterRequestDTO.cs
+++ DTOs/RegisterRequestDTO.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 public class RegisterRequestDTO
 {
+    [Required]
+    [MinLength(3)]
     public string userName {get; set;} = string.Empty;
-
+    [Required]
+    [MaxLength(100)]
+    [EmailAddress]
     public string userEmail {get; set;} = string.Empty;
-
+    [Required]
+    [MinLength(8)]
     public string password {get; set;} = string.Empty;
 }
